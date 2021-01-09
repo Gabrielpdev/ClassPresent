c1c4beeaa903dfe6932d440db839fdf03d76c059
Gabriel Pereira <gabriel9938@gmail.com>
2021-01-09 12:22:06 -0300

    Adding a database url

--- ormconfig.js
+++ ormconfig.js
@@ -2,6 +2,7 @@ module.exports = {
   type: "postgres",
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
+  url: process.env.DATABASE_URL,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_DATABASE,
