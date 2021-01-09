36a0d4ceccd656e3a2a0c7d5035dd5932fc355fe
Gabriel Pereira <gabriel9938@gmail.com>
2021-01-09 12:39:10 -0300

    changing ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,17 +1,24 @@
 module.exports = {
-  type: "postgres",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   url: process.env.DATABASE_URL,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE,
-  migrations: [process.env.TYPEORM_MIGRATIONS],
-  entities: [process.env.TYPEORM_ENTITIES],
-  cli: {
-    migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR
+    type: 'postgres',
+    entities: [process.env.TYPEORM_ENTITIES],
+    synchronize: true,
+    extra: {
+      ssl: true,
   },
-  extra: {
-    ssl: true
-  }
+  // type: "postgres",
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // url: process.env.DATABASE_URL,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database: process.env.DB_DATABASE,
+  // migrations: [process.env.TYPEORM_MIGRATIONS],
+  // entities: [process.env.TYPEORM_ENTITIES],
+  // cli: {
+  //   migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR
+  // },
+  // extra: {
+  //   ssl: true
+  // }
 }
