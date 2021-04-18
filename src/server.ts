79203f3dc42f9c1060ca9676607054eb22e089f4
Gabriel Oliveira <gabriel9938@gmail.com>
2021-04-18 12:42:43 -0300

    removendo comentarios

--- src/server.ts
+++ src/server.ts
@@ -11,17 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-// app.listen(process.env.PORT, '0.0.0.0');
-
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Server started on port ${process.env.PORT || 8080} 🚀`)
 });
-
-// app.set('port', (process.env.APP_PORT || 5000));
-// //For avoidong Heroku $PORT error
-// app.get('/', function(request, response) {
-//   var result = 'App is running'
-//   response.send(result);
-// }).listen(app.get('port'), function() {
-//   console.log('App is running, server is listening on port ', app.get('port'));
-// });
